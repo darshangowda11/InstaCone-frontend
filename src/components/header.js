ee63fc31709fc560860713ce14f6f8bd50a8b5b8
Darshan <109055047+darshangowda11@users.noreply.github.com>
2023-01-24 17:20:57 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -8,6 +8,8 @@ export default function Header(){
     return (
         <div className="header">
             <h1 className="topleft"><ImInstagram/></h1>
+            <h1 className="color">INSTA-APP</h1>
+            {/* <img  src="./instathumb.jpg" alt="insta-logo"/> */}
             <h1 className="topright"><Link to = "/uploadview" ><BsFillCameraFill/></Link></h1>
         </div>
     )
